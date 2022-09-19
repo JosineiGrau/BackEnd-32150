e50c224bd29e74ab9ed28bec8485f37f52563666
JosineiGrau <jgrausalazar@gmail.com>
2022-09-19 15:50:34 -0500

    Desafio 2 clase container con metodos asincronos

---
+++ desafio2/main.js
@@ -0,0 +1,11 @@
+const Container = require("./container")
+
+const product = new Container
+    product.read()
+setTimeout(()=>{
+    // product.save({name:"cart",price:15})
+    // console.log(product.getById(1));
+    // console.log(product.getAll());
+    // product.deleteById(1)
+    // product.deleteAll()
+},1000)
