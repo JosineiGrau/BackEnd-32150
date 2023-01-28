bdc589821bcc9b9da95ed40a3cb586310edc23d6
JosineiGrau <jgrausalazar@gmail.com>
2023-01-28 17:21:49 -0500

    Agregando la api de twilio

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,3 +1,4 @@
+const { transporter, mailOptions, emailTemplateCheckout } = require("../config/nodemailer.config");
 const client = require("../config/twilio.config");
 const success = require("../networks/responses");
 const Carts = require('../services/daos/carts/cartsMongo.daos');
@@ -46,27 +47,30 @@ const postAddProductToCartController = async (req, res, next) => {
 
 const postBuyItemsController = async (req, res, next) => {
     try {
-		const { name, email, photo } = req.user
+		const { name, email } = req.user
 		const { cartId } = req.params;
 		const cartById = await db.getById(cartId);
 
 		// GMAIL
-		// await transporter.sendMail(mailOptions(emailTemplateCheckout(cartById[0].products), `Nuevo pedido de ${name}, ${email}`))
+		await transporter.sendMail(mailOptions(emailTemplateCheckout(cartById[0].products), `Nuevo pedido de ${name}, ${email}`))
 
 		// SMS
-		// await client.messages.create({
-		// 	body: `Hola ${name}, su pedido ha sido recibido y se encuentra en proceso}`,
-		// 	from: "+17122144461",
-		// 	to: "+51940471501"
-		// })
+		await client.messages.create({
+			body: `Hola ${name}, su pedido ha sido recibido y se encuentra en proceso}`,
+			from: "+17122144461",
+			to: "+51940471501"
+		})
 
 		// WhatsApp
-		await client.messages.create({
-			mediaUrl: [cartById[0].products.map((item) => item.image)],
+		cartById[0].products.forEach(async (element) => {
+			await client.messages.create({
+			mediaUrl: element.image,
 			body: `Hola ${name}, su pedido ha sido recibido y se encuentra en proceso}`,
 			from: "whatsapp:+14155238886",
 			to: "whatsapp:+51977944283"
 		})
+		});
+		
 		success(res, 202, 'Compra realizada')
 
 	} catch (err) {
